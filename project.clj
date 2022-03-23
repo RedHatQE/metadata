b3e65fb6cf1fac46cca4fb2c276cdf22943bc6c1
Craig Donnelly <crag@redhat.com>
2022-03-22 23:28:18 -0400

    Migrate project to clojars

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject com.github.redhatqe/polarize-metadata "0.1.1-SNAPSHOT"
+  :description "Polarize metadata for RHSM TestNG"
+  :url "https://github.com/RedHatQE/metadata"
+  :java-source-path "src"
+  :java-source-paths ["src"]
+  :javac-options {:debug "on"}
+  :plugins [[lein2-eclipse "2.0.0"]])
